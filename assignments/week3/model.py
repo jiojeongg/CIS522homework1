032201ef75aa6aab1a81441abf6622df5ff22e3a
Jio Jeong <jiojeong@sas.upenn.edu>
2023-02-08 01:25:31 -0500

    first

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -2,14 +2,14 @@ import torch
 from typing import Callable
 
 
-class MLP:
+class MLP(torch.nn.Module):
     def __init__(
         self,
         input_size: int,
         hidden_size: int,
         num_classes: int,
-        hidden_count: int = 1,
-        activation: Callable = torch.nn.ReLU,
+        hidden_count: int,
+        activation: Callable = torch.nn.ReLU(),
         initializer: Callable = torch.nn.init.ones_,
     ) -> None:
         """
@@ -22,7 +22,13 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super().__init__()
+
+        self.activation = torch.nn.ReLU()
+
+        self.fc1 = torch.nn.Linear(input_size, hidden_size)
+        self.fc2 = torch.nn.Linear(hidden_size, num_classes)
+        #self.fc3 = torch.nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
         """
@@ -34,4 +40,18 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        # x = [batch size, height, width]
+
+        batch_size = x.shape[0]
+
+        x = x.view(batch_size, -1)
+
+        # x = [batch size, height * width]
+
+        h_1 = self.activation(self.fc1(x))
+
+        #h_2 = self.activation(self.fc2(h_1))
+
+        y_pred = self.activation(self.fc2(h_1))
+
+        return y_pred
