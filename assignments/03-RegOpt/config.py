e45ea42bffd8ccd68229f4aedbfe73cb0c20a714
Jio Jeong <jiojeong@sas.upenn.edu>
2023-02-20 20:10:53 -0500

    real final 64, 10, 0.002

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -10,7 +10,7 @@ class CONFIG:
     class for configuration
     """
 
-    batch_size = 256
+    batch_size = 64
     num_epochs = 10
     initial_learning_rate = 0.002
     initial_weight_decay = 0
