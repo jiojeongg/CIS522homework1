7e84f386733eea1710880e961b2e42737d4d3923
Jio Jeong <jiojeong@sas.upenn.edu>
2023-03-18 21:43:16 -0400

    larger lr

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -17,7 +17,7 @@ class Model(torch.nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
+        self.fc2 = nn.Linear(120, num_classes)
         self.fc3 = nn.Linear(84, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -28,6 +28,6 @@ class Model(torch.nn.Module):
         x = self.pool(F.relu(self.conv2(x)))
         x = torch.flatten(x, 1)  # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        #x = F.relu(self.fc2(x))
+        x = self.fc2(x)
         return x
