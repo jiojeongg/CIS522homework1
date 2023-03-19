f70083ff494d27d609130158ae5c6a83ffd355ac
Jio Jeong <jiojeong@sas.upenn.edu>
2023-03-18 21:42:35 -0400

    larger lr

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -15,6 +15,6 @@ class CONFIG:
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.Adam(model.parameters(), lr=0.007)
+    ] = lambda model: torch.optim.Adam(model.parameters(), lr=0.0087)
 
     transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
